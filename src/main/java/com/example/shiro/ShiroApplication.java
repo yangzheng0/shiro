669d693b1569c533015b6723f7f53abea30f301d
Adam <1069958530@qq.com>
2018-07-11 13:23:00 +0800

    12

---
+++ src/main/java/com/example/shiro/ShiroApplication.java
@@ -0,0 +1,16 @@
+package com.example.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+
+@SpringBootApplication
+public class ShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroApplication.class, args);
+    }
+}
---
+++ src/main/resources/static/js/1.js
@@ -0,0 +1,3 @@
+// JavaScript Document
+document.write("这是我的第一个javascript代码");
+alert("haha");
